167af8cf52cb0003cac6f0506ec6c4cf10094db1
Luke Craig <craigla@rose-hulman.edu>
2017-11-02 13:04:39 -0400

    updated readme and some files

--- scan.py
+++ scan.py
@@ -14,22 +14,21 @@ def check_ssh(ip,user,passwd):
 	except:
 		return False
 
-def main():
-	# parse arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument("ip")
-	parser.add_argument("-aggr","-aggressive", '--aggressive', action="store_true")
-	args = parser.parse_args()
+
+def scan(ip):
 
 	# notify user that the scan is beginning
-	print("Scanning "+args.ip+"...")
+	print("Scanning "+ip+"...")
 	nm = nmap.PortScanner()
-	scan = nm.scan(args.ip,'21-443,3000,8080,9090')
+	scan = nm.scan(ip,'21-443,3000,8080,9090')
 	
 	# parse results
 	command = scan['nmap']
 	stats = command['scanstats']
 	scan_scan = scan['scan']
+	if not scan_scan:
+		print("Scan failed. No results returned. Check that host is up.")
+		return
 	results = scan_scan[scan_scan.keys()[0]] # this is the ip address
 	relevant_results = results['tcp'] 		 # these indicate open ports
 
@@ -40,14 +39,14 @@ def main():
 		if args.aggressive:
 			pwd_map = [("root",""),("root","temppwd"),("debian",""),("debian","temppwd")]
 			for i in pwd_map:
-				works = check_ssh(args.ip,i[0],i[1])
+				works = check_ssh(ip,i[0],i[1])
 				print("attempt: "+str(i)+" "+works)
 	# Checking HTTP Port 80
 	if 80 in relevant_results:
 		print("Port 80: http is open")
 
 		#lets check to see if its Bone101
-		result = wget("http://"+args.ip, timeout=20)
+		result = wget("http://"+ip, timeout=20)
 		if "Bone101" in result:
 			print("Server running open Bone101.")
 			if args.aggressive:
@@ -58,7 +57,7 @@ def main():
 			
 	if 3000 in relevant_results:
 		print("Port 3000 open")
-		result = wget("http://"+args.ip+":3000",timeout=20)
+		result = wget("http://"+ip+":3000",timeout=20)
 		if "Cloud9" in result:
 			print("Server running Cloud9.")
 			if args.aggressive:
@@ -69,6 +68,26 @@ def main():
 		#42["bonescript$writeTextFile",{"filename":"/home/debian/out.txt","data":"heartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\nheartbeat\ndatatoappend\n","seq":13}]
 	if 8080 in relevant_results:
 		print("apache httpd running on port 8080")
+
+def main():
+	# parse arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("ip")
+	parser.add_argument("-aggr","-aggressive", '--aggressive', action="store_true")
+	parser.add_argument("-mean","-mean","--mean",action="store_true")
+	args = parser.parse_args()
+
+	if "*" in args.ip:
+		ip = ".".join(args.ip.split(".")[:3])+"."
+		for i in range(1,256):
+			if "*" in ip:
+				print("we are not scanning over anything bigger than a subnet")
+				return
+			scan(ip+str(i))
+
+	else:
+		scan(args.ip)
+
 
 if __name__ == "__main__":
 	main()
