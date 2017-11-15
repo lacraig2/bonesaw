6f15652a9b259a2298d3aefbf1b5e6bd15194892
Luke Craig <craigla@rose-hulman.edu>
2017-11-14 20:55:48 -0500

    bonesaw install update

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+sudo pip2 install python-nmap
+sudo pip install --upgrade pip
+sudo pip install --upgrade pwntools
