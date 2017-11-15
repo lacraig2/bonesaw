b20159c93227d0020ad601b8152e5f9072f52b38
Luke Craig <lacraig3@gmail.com>
2017-11-14 21:00:23 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
-sudo pip2 install python-nmap
-sudo pip install --upgrade pip
-sudo pip install --upgrade pwntools
