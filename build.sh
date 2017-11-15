13b41654f7db74c1bb6d58850943ada9c717c1f8
Luke Craig <lacraig3@gmail.com>
2017-11-14 21:00:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
-# we build nmap from source. get over it.
-wget https://nmap.org/dist/nmap-7.60.tar.bz2
-bzip2 -cd nmap-7.60.tar.bz2 | tar xvf -
-cd nmap-7.60
-./configure
-make
-su root
-make install
-exit
-cd ..
+sudo apt-get update
+sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+sudo pip2 install python-nmap
+sudo pip install --upgrade pip
+sudo pip install --upgrade pwntools
